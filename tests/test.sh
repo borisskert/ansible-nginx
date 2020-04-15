6f3a403f21cd0eca2506eb2c1544e9f2039a0240
Boris Skert <boris.skert@gmail.com>
2020-04-15 16:16:57 +0200

    Clean up systemd services; Improve code

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,11 @@ vagrant up --provision
 
 ansible-galaxy install -r requirements.yml -p ./roles --force
 
+echo "Waiting for answer on port 22..."
+while ! timeout 1 nc -z 192.168.33.26 22; do
+  sleep 0.2
+done
+
 ansible-playbook -i inventory.ini test.yml
 
 ansible-playbook -i inventory.ini test.yml \
@@ -12,7 +17,14 @@ ansible-playbook -i inventory.ini test.yml \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
 
-curl -s http://192.168.33.10:80 \
+echo "Waiting for nginx answer ..."
+while ! timeout 1 nc -z 192.168.33.26 80; do
+  sleep 0.2
+done
+
+curl -s http://192.168.33.26:80 \
  | grep -q '</html>' \
   && (echo 'curl test: pass' && exit 0) \
   || (echo 'curl test: fail' && exit 1)
+
+vagrant destroy -f
